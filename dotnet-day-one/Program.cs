b79bc9b356a7d36e82d3f3e2fe7b8be8e9fca8b3
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-11-17 12:41:47 +0100

    :eyes: beautiful namespaces, i must say :eyes:

--- dotnet-day-one/Program.cs
+++ dotnet-day-one/Program.cs
@@ -1,4 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("THis is going to be fun");
 Console.WriteLine("THis is going to be a very fun project.");
+
+// Shit-posting style of slinging
+// codes le'ts see what we can knock 
+// down in here. 
+// - Variables
+// - Conditionals
+// - Some other sheninigans
+namespace ThereIsNothingMore
+{
+
+
+}
+
