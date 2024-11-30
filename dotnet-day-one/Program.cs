656025dab0be6c8619edfac94ade86cc55c037b1
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-11-30 20:28:15 +0100

    some updates

--- dotnet-day-one/Program.cs
+++ dotnet-day-one/Program.cs
@@ -14,6 +14,24 @@ Console.WriteLine("THis is going to be a very fun project.");
 namespace ThereIsNothingMore
 {
 
+    internal class MeAndYouTonight { }
+    internal class DateNight { }
+    internal class Movies { }
+    internal class Bowling { }
+    internal class ThatWhichMustNotBeNamed { }
+
+
+    class Program
+    {
+
+        // We would see something happening in here, and if it burns
+        // we would watch it fail, burn, panic for pleasure andd recover
+        // it remains
+        static void Main(String[] args)
+        {
+
+        }
+    }
 
 }
 
